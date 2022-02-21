b25faf8087a1e7c1506890eeefce184f822f4468
Limonkaa <90504872+Limonkaa@users.noreply.github.com>
2022-02-21 23:49:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('#mline').click(function() {
+    $('.head').slideToggle();
+});
+})
+
+$('document').ready(function(){
+    $('#h').click(function(){
+        $('.h').slideToggle();
+
+    })
+    $('#nh').click(function(){
+        $('.nh').slideToggle();
+
+    })
+    $('#child').click(function(){
+        $('.child').slideToggle();
+
+    })
+})
