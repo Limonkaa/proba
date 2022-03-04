7e01de33894cc6b86be3b61becf5dcacd57981b7
Limonkaa <90504872+Limonkaa@users.noreply.github.com>
2022-03-04 13:50:36 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,237 @@
+$(document).ready(function(){
+$('#mline').click(function() {
+    $('.head').slideToggle();
+});
+})
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog1" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener1" ).on( "click", function() {
+      $( "#dialog1" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog2" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener2" ).on( "click", function() {
+      $( "#dialog2" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog3" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener3" ).on( "click", function() {
+      $( "#dialog3" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog4" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener4" ).on( "click", function() {
+      $( "#dialog4" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog5" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener5" ).on( "click", function() {
+      $( "#dialog5" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog6" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener6" ).on( "click", function() {
+      $( "#dialog6" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog7" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener7" ).on( "click", function() {
+      $( "#dialog7" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog8" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener8" ).on( "click", function() {
+      $( "#dialog8" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog9" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener9" ).on( "click", function() {
+      $( "#dialog9" ).dialog( "open" );
+    });
+  } );
+
+  $( function() {
+    $( "#dialog10" ).dialog({
+      autoOpen: false,
+      modal:true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener10" ).on( "click", function() {
+      $( "#dialog10" ).dialog( "open" );
+    });
+  } );
+
+
+
+ 
+  
+  
